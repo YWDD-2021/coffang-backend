898400a70f6158fbac5ccd341e206e8d97d074ad
egyeasy <dz1120@gmail.com>
2021-08-14 22:47:31 +0900

    fix Item inheritance error

--- src/main/java/com/coffang/springboot2_coffang/domain/item/Brewing.java
+++ src/main/java/com/coffang/springboot2_coffang/domain/item/Brewing.java
@@ -2,11 +2,13 @@ package com.coffang.springboot2_coffang.domain.item;
 
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 
 @Entity
+@EqualsAndHashCode(callSuper = true)
 @Data
 public class Brewing extends Item{
 
