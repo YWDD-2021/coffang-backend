38f99a52240c7827668579e07411c0fa3ee20cfc
egyeasy <dz1120@gmail.com>
2021-08-28 23:22:14 +0900

    add JsonCreater to Brewing, Serveware

--- src/main/java/com/coffang/springboot2_coffang/domain/item/Brewing.java
+++ src/main/java/com/coffang/springboot2_coffang/domain/item/Brewing.java
@@ -1,6 +1,8 @@
 package com.coffang.springboot2_coffang.domain.item;
 
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Data;
@@ -14,6 +16,19 @@ import javax.persistence.Entity;
 @Data
 @JsonDeserialize(using= JsonDeserializer.None.class)
 public class Brewing extends Item{
+
+    @JsonCreator
+    public Brewing(@JsonProperty("id") Long id,
+                  @JsonProperty("name") String name,
+                  @JsonProperty("price") Long price,
+                  @JsonProperty("stock_quantity") Integer stockQuantity,
+                  @JsonProperty("category") String category,
+                  @JsonProperty("image_url") String imageUrl,
+                  @JsonProperty("tool_type") String toolType
+    ) {
+        super(id, name, price, stockQuantity, category, imageUrl);
+        this.toolType = toolType;
+    }
 
     @Column(nullable = false,length=100)
     String toolType;
--- src/main/java/com/coffang/springboot2_coffang/domain/item/Serveware.java
+++ src/main/java/com/coffang/springboot2_coffang/domain/item/Serveware.java
@@ -1,5 +1,7 @@
 package com.coffang.springboot2_coffang.domain.item;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Data;
@@ -14,6 +16,20 @@ import javax.persistence.Entity;
 @JsonDeserialize(using= JsonDeserializer.None.class)
 public class Serveware extends Item {
     //id,name,price,stockQuantity,category,imageUrl 상속
+
+    @JsonCreator
+    public Serveware(@JsonProperty("id") Long id,
+                  @JsonProperty("name") String name,
+                  @JsonProperty("price") Long price,
+                  @JsonProperty("stock_quantity") Integer stockQuantity,
+                  @JsonProperty("category") String category,
+                  @JsonProperty("image_url") String imageUrl,
+                  @JsonProperty("type") String type,
+                  @JsonProperty("brand") String brand
+    ) {
+        super(id, name, price, stockQuantity, category, imageUrl);
+        this.brand = brand;
+    }
 
     @Column(nullable= false,length=100)
     String brand;
