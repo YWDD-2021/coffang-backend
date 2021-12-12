b4b2b04b23225c5e425f31e1168eb76b17ba3635
ChipmunkForLove <chipmunk4lov@gmail.com>
2021-12-12 11:36:51 +0900

    [Feat] indexController 추가

--- src/main/java/com/coffang/springboot2_coffang/controller/IndexController.java
+++ src/main/java/com/coffang/springboot2_coffang/controller/IndexController.java
@@ -1,13 +1,22 @@
 package com.coffang.springboot2_coffang.controller;
 
+import com.coffang.springboot2_coffang.config.auth.dto.SessionUser;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final HttpSession httpSession;
+
     @GetMapping("/")
     public String index() {
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         return "index";
     }
+
 }
