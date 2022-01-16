8a20dd2a0b36ea275132204b956af60fa724474d
egyeasy <35281673+egyeasy@users.noreply.github.com>
2022-01-16 12:40:08 +0900

    Update src/main/java/com/coffang/springboot2_coffang/Springboot2CoffangApplication.java

--- src/main/java/com/coffang/springboot2_coffang/Springboot2CoffangApplication.java
+++ src/main/java/com/coffang/springboot2_coffang/Springboot2CoffangApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
-public class                                               Springboot2CoffangApplication {
+public class Springboot2CoffangApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Springboot2CoffangApplication.class, args);
