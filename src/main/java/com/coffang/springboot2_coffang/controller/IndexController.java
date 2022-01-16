33d79dea6b55e912796594bfe3941228d46d3b22
ChipmunkForLove <chipmunk4lov@gmail.com>
2022-01-16 12:29:14 +0900

    [Feat] rds springboot 와 연동하도록 설정 변경, controller에서 json형식 전달하도록 변경

---
+++ src/main/java/com/coffang/springboot2_coffang/controller/IndexController.java
@@ -0,0 +1,25 @@
+package com.coffang.springboot2_coffang.controller;
+
+import com.coffang.springboot2_coffang.config.auth.dto.SessionUser;
+import com.coffang.springboot2_coffang.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+    private final HttpSession httpSession;
+    private final ItemService itemService;
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("items", itemService.findAll());
+        return "index";
+    }
+
+}
